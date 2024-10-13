dbf21fb55abd9a35feeeb8f2199e4a75fd486e5d
sebalooc35 <andres.ordonez@galileo.edu>
2024-10-13 13:23:44 -0600

    fix port problem

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(80, '0.0.0.0', () => {
-    console.log('Listening on port 80');
+server.listen(8000, '0.0.0.0', () => {
+    console.log('Listening on port 8000');
 });
